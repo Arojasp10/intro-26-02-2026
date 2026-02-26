4d6e314be1fce2fa0a8c5cd97358e837f9cfe3a9
Arojasp10 <alexasagoru@gmail.com>
2026-02-26 10:01:43 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,6 @@
 import streamlit as st
+from PIL import image
 
 st.title("Esta es mi primera app en la nube")
+st.header("En este espacio va un texto largo")
+st.write("Reazlizar algo de backend y frondend")
