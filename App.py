16f7ba4068827cfa697c05863eede310a37876da
Arojasp10 <alexasagoru@gmail.com>
2026-02-26 10:16:50 -0500

    Update App.py

--- App.py
+++ App.py
@@ -6,3 +6,6 @@ st.header("En este espacio va un texto largo")
 st.write("Reazlizar algo de backend y frondend")
 image = Image.open("FondoObsidian.png")
 st.image(image, caption="Fondo Obsidian") 
+
+texto = st.text_input("Escriba algo", "Este es el texto")
+st.write("El texto escrito es", texto)
