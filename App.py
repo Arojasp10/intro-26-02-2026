dd7c4f75ba52d28381abfdb143a0ec027fdf55de
Arojasp10 <alexasagoru@gmail.com>
2026-02-26 10:03:29 -0500

    Update App.py

--- App.py
+++ App.py
@@ -4,3 +4,5 @@ from PIL import image
 st.title("Esta es mi primera app en la nube")
 st.header("En este espacio va un texto largo")
 st.write("Reazlizar algo de backend y frondend")
+image = Image.open("FondoObsidian.png")
+st.image(Image, caption="Fondo Obsidian") 
