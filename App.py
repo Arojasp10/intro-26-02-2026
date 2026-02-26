d9e6888e9c064f51a66cc5c13be8f7ae59264947
Arojasp10 <alexasagoru@gmail.com>
2026-02-26 10:24:54 -0500

    Update App.py

--- App.py
+++ App.py
@@ -9,3 +9,15 @@ st.image(image, caption="Fondo Obsidian")
 
 texto = st.text_input("Escriba algo", "Este es el texto")
 st.write("El texto escrito es", texto)
+
+st.subheader("ahora dos col")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la 1era columna")
+  st.write("Algo de las interfaces")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("Correcto")
+
